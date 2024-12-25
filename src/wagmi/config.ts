6e0b95c8ea5580c368c139e4885e59926abc4b4d
Abdullah Furkan Özbek <furkanozbek1995@gmail.com>
2024-12-25 18:39:29 +0300

    config updated

--- src/wagmi/config.ts
+++ src/wagmi/config.ts
@@ -1,11 +1,19 @@
-import { http, createConfig, createStorage, cookieStorage } from "wagmi";
+import {
+  http,
+  createConfig,
+  createStorage,
+  cookieStorage,
+  injected,
+} from "wagmi";
 import { baseSepolia } from "viem/chains";
+import { metaMask } from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [baseSepolia],
   transports: {
     [baseSepolia.id]: http(),
   },
+  connectors: [injected(), metaMask()],
   // ssr: true,
   // storage: createStorage({
   //   storage: cookieStorage,
