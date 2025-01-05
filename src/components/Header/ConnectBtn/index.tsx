44adadecef7ec20d068e4ef61ea358ff20bb22a9
Abdullah Furkan Özbek <abdullah.furkan.ozbek@gmail.com>
2025-01-05 23:03:40 +0300

    connectors expanded

--- src/components/Header/ConnectBtn/index.tsx
+++ src/components/Header/ConnectBtn/index.tsx
@@ -1,13 +1,25 @@
 "use client";
 
+import { useToast } from "@/app/hooks/useToast";
+import { config } from "@/wagmi/config";
 import { baseSepolia } from "viem/op-stack";
-import { injected, useConnect } from "wagmi";
+import { useConnect } from "wagmi";
+import { getAccount } from "wagmi/actions";
 
 export default function ConnectBtn() {
   const { connect } = useConnect();
+  const { error } = useToast();
 
   const connectAccount = async () => {
-    await connect({ connector: injected(), chainId: baseSepolia.id });
+    // gets the current connector
+    const { connector } = getAccount(config);
+
+    if (connector) {
+      // We will use baseSepolia chain for this project (You may change it to base mainnet on production)
+      await connect({ connector, chainId: baseSepolia.id });
+    } else {
+      error("Please install Metamask to connect your wallet");
+    }
   };
 
   return (
