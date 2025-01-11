df954e2d761633e3b461270ab1cc48ce9b583486
Abdullah Furkan Özbek <abdullah.furkan.ozbek@gmail.com>
2025-01-11 15:24:58 +0300

    Transactions with suspense added

--- src/components/Staking/index.tsx
+++ src/components/Staking/index.tsx
@@ -1,12 +1,25 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StakingForm from "./StakingForm";
 import { useAccount } from "wagmi";
 import StakingConnectCTA from "./StakingConnectCTA";
+import StakingFormSkeleton from "./StakingFormSkeleton";
 
 const StakingContainer = () => {
-  const { address, isConnected } = useAccount();
+  const { address, isConnected, isConnecting } = useAccount();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    if (!isConnecting) {
+      setMounted(true);
+    }
+  }, [isConnecting]);
+
+  // Show StakingFormSkeleton while mounting
+  if (!mounted) {
+    return <StakingFormSkeleton />;
+  }
 
   return (
     <>{address && isConnected ? <StakingForm /> : <StakingConnectCTA />}</>
