f2e662c08d35e7939ddf0b4652de63cf72d1992e
Abdullah Furkan Özbek <abdullah.furkan.ozbek@gmail.com>
2025-01-21 11:32:40 +0300

    fixed metamask ssr error by rendering it on client

--- src/wagmi/config.ts
+++ src/wagmi/config.ts
@@ -1,12 +1,16 @@
-import { http, createConfig, injected } from "wagmi";
+"use client";
+
+import { http, createConfig } from "wagmi";
 import { baseSepolia } from "viem/chains";
 import { metaMask } from "wagmi/connectors";
+
+const connectors = typeof window !== "undefined" ? [metaMask()] : [];
 
 export const config = createConfig({
   chains: [baseSepolia],
   transports: {
     [baseSepolia.id]: http(),
   },
-  connectors: [injected(), metaMask()],
+  connectors,
   ssr: true,
 });
